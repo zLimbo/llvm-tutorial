33c0773c7268e61f2f327f1ae8c2bb71826021aa
zlimbo <z_limbo@foxmail.com>
2023-02-23 20:10:46 +0800

    3. Kaleidoscope: Code generation to LLVM IR

--- 3/toy.cpp
+++ 3/toy.cpp
@@ -1,5 +1,12 @@
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Value.h"
+#include "llvm/IR/Verifier.h"
 #include <cctype>
 #include <cstdio>
+#include <map>
 #include <string>
 #include <memory>
 #include <vector>
@@ -92,6 +99,7 @@ static int gettok() {
 class ExprAST {
 public:
     virtual ~ExprAST() = default;
+    virtual llvm::Value *codegen() = 0;
 };
 
 /// NumberExprAST - Expression class for numeric literals like "1.0".
@@ -101,6 +109,8 @@ private:
 
 public:
     NumberExprAST(double Val) : Val(Val) {}
+
+    llvm::Value *codegen() override;
 };
 
 /// VariableExprAST - Expression class for referencing a variable, like "a".
@@ -109,6 +119,8 @@ private:
     std::string Name;
 public:
     VariableExprAST(const std::string &Name) : Name(Name) {}
+
+    llvm::Value *codegen() override;
 };
 
 /// BinaryExprAST - Expression class for a binary operator.
@@ -120,6 +132,8 @@ public:
     BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
                     std::unique_ptr<ExprAST> RHS)
         : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+
+    llvm::Value *codegen() override;
 };
 
 /// CallExprAST - Expression class for function calls.
@@ -131,6 +145,8 @@ public:
     CallExprAST(const std::string &Callee,
                 std::vector<std::unique_ptr<ExprAST>> Args)
         : Callee(Callee), Args(std::move(Args)) {}
+
+    llvm::Value *codegen() override;
 };
 
 
@@ -146,6 +162,7 @@ public:
     PrototypeAST(const std::string &Name, std::vector<std::string> Args)
         : Name(Name), Args(std::move(Args)) {}
     
+    llvm::Function *codegen();
     const std::string &getName() const { return Name; }
 };
 
@@ -159,6 +176,8 @@ public:
     FunctionAST(std::unique_ptr<PrototypeAST> Proto,
                 std::unique_ptr<ExprAST> Body)
         : Proto(std::move(Proto)), Body(std::move(Body)) {}
+    
+    llvm::Function *codegen();
 };
 
 
@@ -405,6 +424,156 @@ static std::unique_ptr<FunctionAST> ParseTopLevelExpr() {
     }
     return nullptr;
 }
+
+//===------------------===//
+// Code Generation
+//===------------------===//
+
+// TheContext is an opaque object that owns a lot of core LLVM data structres,
+// such as the type and constant value tables.
+static std::unique_ptr<llvm::LLVMContext> TheContext;
+
+// TheModule is an LLVM construct that contains functions and global variables.
+// The LLVM Module is the container that holds the functions we are JIT'ing.
+static std::unique_ptr<llvm::Module> TheModule;
+
+// The Builder object is a helper object that makes it easy to gennerate LLVM instructions.
+// Instances of the IRBuilder class template keep track of the current place to insert instructions
+// and has methods to create new instructions.
+static std::unique_ptr<llvm::IRBuilder<>> Builder;
+
+// The NamedValue map keeps track of which values are defined in the current scope
+// and what their LLVM representation is.
+static std::map<std::string, llvm::Value *> NamedValues;
+
+llvm::Value *LogErrorV(const char *Str) {
+    LogError(Str);
+    return nullptr;
+}
+
+llvm::Value *NumberExprAST::codegen() {
+    // Use "foo::get(...)": that constants are all uniqued together and shared.
+    return llvm::ConstantFP::get(*TheContext, llvm::APFloat(Val));
+}
+
+llvm::Value *VariableExprAST::codegen() {
+    // Look this variable up in the function.
+    llvm::Value *V = NamedValues[Name];
+    if (!V) {
+        LogErrorV("Unknown variable name");
+    }
+    return V;
+}
+
+llvm::Value *BinaryExprAST::codegen() {
+    llvm::Value *L = LHS->codegen();
+    llvm::Value *R = RHS->codegen();
+    if (!L || !R) {
+        return nullptr;
+    }
+    switch (Op) {
+        case '+':
+            return Builder->CreateFAdd(L, R, "addtmp");
+        case '-':
+            return Builder->CreateFSub(L, R, "subtmp");
+        case '*':
+            return Builder->CreateFMul(L, R, "multmp");
+        case '<':
+            L = Builder->CreateFCmpULT(L, R, "cmptmp");
+            // Convert bool 0/1 to double 0.0 or 1.0
+            return Builder->CreateUIToFP(L, llvm::Type::getDoubleTy(*TheContext), 
+                                        "booltmp");
+        default:
+            return LogErrorV("invalid binary operator");
+    }
+}
+
+llvm::Value *CallExprAST::codegen() {
+    // Look up the name in the global module table.
+    llvm::Function *CalleeF = TheModule->getFunction(Callee);
+    if (!CalleeF) {
+        return LogErrorV("Unknown function referenced");
+    }
+
+    // If argument mismatch error.
+    if (CalleeF->arg_size() != Args.size()) {
+        return LogErrorV("Incorrect # argument passed");
+    }
+
+    std::vector<llvm::Value *> ArgsV;
+    for (unsigned i = 0, e = Args.size(); i != e; ++i) {
+        ArgsV.push_back(Args[i]->codegen());
+        if (!ArgsV.back()) {
+            return nullptr;
+        }
+    }
+    return Builder->CreateCall(CalleeF, ArgsV, "calltmp");
+}
+
+
+llvm::Function *PrototypeAST::codegen() {
+    // Make the function type: double(double, double) etc.
+    std::vector<llvm::Type*> Doubles(Args.size(), llvm::Type::getDoubleTy(*TheContext));
+    llvm::FunctionType *FT = llvm::FunctionType::get(llvm::Type::getDoubleTy(*TheContext),
+                                                    Doubles, false);
+    llvm::Function *F = llvm::Function::Create(FT, llvm::Function::ExternalLinkage, 
+                                                Name, TheModule.get());
+    // Set names for all arguments
+    unsigned Idx = 0;
+    for (auto &Arg: F->args()) {
+        Arg.setName(Args[Idx++]);
+    }                            
+    return F;                    
+}
+
+
+llvm::Function *FunctionAST::codegen() {
+    // First, check for an existing function from a previous 'extern' declaration.
+    llvm::Function *TheFunction = TheModule->getFunction(Proto->getName());
+
+    if (!TheFunction) {
+        TheFunction = Proto->codegen();
+    }
+
+    if (!TheFunction) {
+        return nullptr;
+    }
+
+    if (!TheFunction->empty()) {
+        return (llvm::Function*)LogErrorV("Function cannot be redefined.");
+    }
+
+    // Create a new basic block to start insertion into.
+    llvm::BasicBlock *BB = llvm::BasicBlock::Create(*TheContext, "entry", TheFunction);
+    Builder->SetInsertPoint(BB);
+
+    // Record the function arguments in the NameValues map.
+    NamedValues.clear();
+    for (auto &Arg : TheFunction->args()) {
+        NamedValues[std::string(Arg.getName())] = &Arg;
+    }
+
+    if (llvm::Value *RetVal = Body->codegen()) {
+        // Finish off the function.
+        Builder->CreateRet(RetVal);
+
+        // Validate the generated code, checking for consistency
+        llvm::verifyFunction(*TheFunction);
+
+        return TheFunction;
+    }
+
+    // Error reading body, remove function
+    TheFunction->eraseFromParent();
+    return nullptr;
+}
+
+
+
+
+
+
+
 
 
 //====-------------------===//
